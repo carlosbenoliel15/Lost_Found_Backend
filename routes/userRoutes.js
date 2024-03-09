1218063fb13550bf292b4171e3de725d69848ae4
dgvmiranda <fc58631@alunos.fc.ul.pt>
2024-03-09 13:05:56 +0000

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,13 +5,9 @@ const ownerController = require('../controllers/ownerController');
 
 // Rota para criar um novo usuário
 router.post('/signup', userController.createUser);
-
 router.put('/update/:id', userController.updateUserById);  
-
 router.delete('/delete/:id', userController.deleteUserById);
-
 router.get('/profile/:id',userController.getUserInfo);
-
 //router.get('/findByEmail/:email', userController.
 
 //Routes for owner
