d1541566ce47189b9871aa653c1c370c24f64eae
dgvmiranda <fc58631@alunos.fc.ul.pt>
2024-05-05 13:39:07 +0100

    Update server.js
    added endpoint health checks

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
 
 app.listen(PORT, () => {
   console.log(`Server runnning on port ${PORT}`);
