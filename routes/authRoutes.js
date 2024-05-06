a4a7e7690756ae69efb6b8f72da69b577f52b75e
dgvmiranda <fc58631@alunos.fc.ul.pt>
2024-05-06 19:13:04 +0100

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,7 @@ const authController = require('../controllers/authController');
 
 // Rota para login
 router.post('/login', authController.login);
+// Rota para login com Google
+router.post('/loginGoogle', authController.loginGoogle);
 
 module.exports = router;
