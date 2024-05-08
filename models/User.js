7ae8b41bc38b659c696d90f2d57763c4ea5cf451
Dong2196 <grcdomingues21@gmail.com>
2024-05-08 15:02:41 +0100

    comit

--- models/User.js
+++ models/User.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema({
   profile_photo:{type:String},
   password: { type: String, required: true },
   phone:String,
-  birth:{type:Date},
+  birth:{type: String},
   status: String,
   nic: { type: String, unique: true },
   nif: { type: String, unique: true },
@@ -17,23 +17,7 @@ const UserSchema = new mongoose.Schema({
   profileImage: String,
 });
 
-// Middleware to format birth date before saving
-UserSchema.pre('save', function(next) {
-  if (this.birth) {
-    // Format birth date to DD-MM-YYYY
-    const formattedBirthDate = formatDate(this.birth);
-    this.birth = formattedBirthDate;
-  }
-  next();
-});
 
-// Function to format date to DD-MM-YYYY
-function formatDate(date) {
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear();
-  return `${day}-${month}-${year}`;
-}
 
 const UserModel = mongoose.model('User', UserSchema);
 
