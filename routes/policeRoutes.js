0eda9a9f92d6704976b7ee6a48701edfcede852c
dgvmiranda <fc58631@alunos.fc.ul.pt>
2024-05-08 16:10:03 +0100

    Update policeRoutes.js

--- routes/policeRoutes.js
+++ routes/policeRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {
   createPoliceStation,
+  getPoliceStation,
   updatePoliceStation,
   getPoliceStationById,
   deletePoliceStation,
