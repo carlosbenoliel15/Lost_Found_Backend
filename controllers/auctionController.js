16dbfc76718d91468119be61854821de0d1e633e
dgvmiranda <fc58631@alunos.fc.ul.pt>
2024-05-31 20:45:48 +0100

    Update auctionController.js

--- controllers/auctionController.js
+++ controllers/auctionController.js
@@ -1,5 +1,6 @@
 const {AuctionModel, BidModel} = require('../models/Auction');
 const {UserModel, BidderModel} = require('../models/User');
+const {FoundObjectModel} = require('../models/Object');
 const ObjectId = require('mongoose').Types.ObjectId;
 
 const errorHandler = (res, error) => {
@@ -25,20 +26,48 @@ exports.createAuction = async (req, res) => {
     }
 }
 
-//*get all auctions
+//get all auctions
 exports.getAllAuctions = async (req, res) => {
     try{
         const auctions = await AuctionModel.find();
         if (!auctions) {
             return res.status(400).json({error: "No auctions found"});
         }
-        return res.status(200).json(auctions);
+
+        var resList = [];
+        for (var i = 0; i < auctions.length; i++){
+            const foundObject = await FoundObjectModel.findById(auctions[i].foundObject);
+            const auction = auctions[i];
+            const bids = await BidModel.find({auction: new ObjectId(auction._id)});
+
+            var highestBid = 0;
+            for (var j = 0; j < bids.length; j++){
+                if (bids[j].value > highestBid){
+                    highestBid = bids[j].value;
+                }
+            }
+            resList.push({
+                _id: auction._id,
+                foundObject: foundObject._id,
+                startDate: auction.startDate, 
+                endDate: auction.endDate,
+                status: auction.status,
+                foundObjectTitle: foundObject.title, 
+                highestBid: highestBid, 
+                bids: bids
+            });
+        }
+
+
+
+
+        return res.status(200).json(resList);
     } catch (error){
         return res.status(400).json({error: "Could not get auctions"});
     }
 }
 
-//*get auction by id
+//get auction by id
 exports.getAuctionById = async (req, res) => {
     try{
         const auction = await AuctionModel.findById(req.params.id);
@@ -51,7 +80,7 @@ exports.getAuctionById = async (req, res) => {
     }
 }
 
-//*update auction by id
+//update auction by id
 exports.updateAuctionById = async (req, res) => {
     try{
         const auction = await AuctionModel.findOne(req.params.id);
@@ -74,7 +103,7 @@ exports.updateAuctionById = async (req, res) => {
     }
 }
 
-//*delete auction by id
+//delete auction by id
 exports.deleteAuctionById = async (req, res) => {
     try{
         const auction = await AuctionModel.findById(req.params.id);
@@ -88,7 +117,7 @@ exports.deleteAuctionById = async (req, res) => {
     }
 }
 
-//*get all auctions by user id
+//get all auctions by user id
 exports.getAllAuctionsByUserId = async (req, res) => {
     try{
         const user = await UserModel.findById(req.params.userid);
